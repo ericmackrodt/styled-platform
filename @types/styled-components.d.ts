f0ce3dfd99d332f428f446026fe27c17b70bb689
Eric Mackrodt <ericmackrodt@gmail.com>
2019-02-01 12:40:09 +1100

    Add files

---
+++ @types/styled-components.d.ts
@@ -0,0 +1,5 @@
+declare module "styled-components/native" {
+  import styled from "styled-components";
+  export * from "styled-components";
+  export default styled;
+}
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+/* tslint:disable no-submodule-imports */
+import {
+  ThemedCssFunction,
+  SimpleInterpolation,
+  css
+} from "styled-components/native";
+import { Platform } from "react-native";
+
+type Platforms<T> = {
+  [key: string]: T;
+  ios: T;
+  android: T;
+};
+
+type PlatformStyles = Platforms<ThemedCssFunction<{}>>;
+
+function generatePlatformConditionalStyle(): PlatformStyles {
+  return {
+    ios: ((
+      strings: TemplateStringsArray,
+      ...interpolations: SimpleInterpolation[]
+    ) =>
+      Platform.OS === "ios" &&
+      css(strings, ...interpolations)) as ThemedCssFunction<{}>,
+    android: ((
+      strings: TemplateStringsArray,
+      ...interpolations: SimpleInterpolation[]
+    ) =>
+      Platform.OS === "android" &&
+      css(strings, ...interpolations)) as ThemedCssFunction<{}>
+  };
+}
+
+export const platform = generatePlatformConditionalStyle();
